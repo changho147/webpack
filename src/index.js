97a82e6ccd875337ee87d107a5a0142494e50167
Changho <afterlife1477@gmail.com>
2022-02-03 09:31:04 +0900

    fixed index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function printHello() {
+    const element = document.createElement("div");
+
+    element.innerHTML = "<h1>Hello Webpack</h1>";
+
+    return element;
+}
+
+document.body.appendChild(printHello());
